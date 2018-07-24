dec6c0ee0fc7ec3ed90f604be7089867a7e85b8f
botour <botour@mail.uni-paderborn.de>
2018-07-24 21:51:07 +0200

    Make project buildable on the infrastructure

---
+++ build/atmapp/src/main/java/de/upb/cs/bibifi/atmapp/ATM.java
@@ -0,0 +1,8 @@
+package de.upb.cs.bibifi.atmapp;
+
+public interface ATM {
+    void createAccount();
+    void deposit();
+    void withdraw();
+    void checkBalance();
+}
---
+++ build/atmapp/src/main/java/de/upb/cs/bibifi/atmapp/Main.java
@@ -0,0 +1,7 @@
+package de.upb.cs.bibifi.atmapp;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello ATM");
+    }
+}
