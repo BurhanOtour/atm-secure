841c8d40ecfd62b77dc33c6a5aa7612635e157cd
sajjadpz <sajjad.pervaiz@hotmail.com>
2018-07-25 09:09:12 +0200

    added basic cli validation to atm

--- build/atmapp/src/main/java/de/upb/cs/bibifi/atmapp/Main.java
+++ build/atmapp/src/main/java/de/upb/cs/bibifi/atmapp/Main.java
@@ -1,7 +1,10 @@
 package de.upb.cs.bibifi.atmapp;
 
+import de.upb.cs.bibifi.atmapp.util.CommandLineHandler;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello ATM");
+        System.out.println("ATM started...");
+        new CommandLineHandler(args).init();
     }
 }
