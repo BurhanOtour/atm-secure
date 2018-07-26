f201f1881cfd9be1a51fd98438f43d69410511f4
Rajat Khanna <rajatkhanna311@gmail.com>
2018-07-26 15:01:27 +0200

    Added socket communication server client

--- build/socket/src/main/java/de/upb/cs/bibifi/client/ClientConnection.java
+++ build/socket/src/main/java/de/upb/cs/bibifi/client/ClientConnection.java
@@ -1,4 +1,38 @@
 package de.upb.cs.bibifi.client;
 
+import java.io.*;
+import java.net.Socket;
+
+
 public class ClientConnection {
+    /**
+     * Runs the client as an application.  First it displays a dialog
+     * box asking for the IP address or hostname of a host running
+     * the date server, then connects to it and displays the date that
+     * it serves.
+     */
+    public static void main(String [] args) throws IOException {
+        clientConnect(3000, "127.0.0.1");
+    }
+
+    public static void clientConnect(int port, String ip) throws IOException {
+        Socket s = new Socket(ip, port);
+        while(true) {
+            String str = "Auth file";
+
+            // Sends data to the output port of the socket
+            OutputStreamWriter os = new OutputStreamWriter(s.getOutputStream());
+            PrintWriter out = new PrintWriter(os);
+            out.write(str);
+            out.flush();
+            out.close();
+            Socket s1 = new Socket(ip, port);
+
+            // Fetches data
+            BufferedReader br = new BufferedReader(new InputStreamReader(s1.getInputStream()));
+            String nickName = br.readLine();
+
+            System.out.println("C: Data from Server: " + nickName);
+        }
+    }
 }
--- build/socket/src/main/java/de/upb/cs/bibifi/server/ServerConnection.java
+++ build/socket/src/main/java/de/upb/cs/bibifi/server/ServerConnection.java
@@ -1,4 +1,38 @@
 package de.upb.cs.bibifi.server;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerConnection {
+    /**
+     * Runs the server.
+     */
+    public static void main(String[] args) throws IOException {
+        serverConnect(3000, "127.0.0.1");
+    }
+
+    public static void serverConnect(int port, String ip) throws IOException {
+        System.out.println("Server is started!");
+        ServerSocket ss = new ServerSocket(port);
+        System.out.println("Server is waiting for client request");
+        Socket s = ss.accept();
+        System.out.println("Client connected!");
+
+        // Fetches data
+        BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        String str = br.readLine();
+
+        System.out.println("Client Data: " + str);
+
+        int response = 200;
+
+        // Sends data
+        OutputStreamWriter os = new OutputStreamWriter(s.getOutputStream());
+        PrintWriter out = new PrintWriter(os);
+        out.write(response);
+        out.flush();
+        System.out.println("S: Response sent from Server to Client");
+        out.close();
+    }
 }
