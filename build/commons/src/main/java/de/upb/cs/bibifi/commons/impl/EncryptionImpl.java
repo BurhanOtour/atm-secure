a3c71f3e144f9e939dd484ef5a1e6a14ff6d40ea
Zain Ulabidin <Zain.Ulabidin@bentley.com>
2018-07-28 13:50:12 +0200

    Fixed changes after pull request

--- build/commons/src/main/java/de/upb/cs/bibifi/commons/impl/EncryptionImpl.java
+++ build/commons/src/main/java/de/upb/cs/bibifi/commons/impl/EncryptionImpl.java
@@ -1,78 +1,81 @@
 package de.upb.cs.bibifi.commons.impl;
 
-import java.io.*;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
+import de.upb.cs.bibifi.commons.IEncryption;
 
 import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-
-import de.upb.cs.bibifi.commons.IEncryption;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 
 public class EncryptionImpl implements IEncryption {
 
-	private static String algoName = "AES";
+    private static String algoName = "AES";
 
-	private static String transformation = "AES/CBC/PKCS5Padding";
+    private static String transformation = "AES/CBC/PKCS5Padding";
 
-	private byte[] key;
+    private byte[] key;
 
-	private static EncryptionImpl singleton;
+    private static EncryptionImpl singleton;
 
-	private EncryptionImpl(){}
+    private EncryptionImpl() {
+    }
 
-	public static EncryptionImpl Initialize(String key){
-		if(singleton == null)
-			singleton = new EncryptionImpl();
+    public static EncryptionImpl initialize(String key) {
+        if (singleton == null)
+            singleton = new EncryptionImpl();
 
-		singleton.key = key.getBytes();
-		return singleton;
-	}
+        singleton.key = key.getBytes();
+        return singleton;
+    }
 
-	public static EncryptionImpl getInstance() throws Exception {
-			if(singleton == null)
-				throw new Exception("Encryption is not initialized");
-		return  singleton;
-	}
+    public static EncryptionImpl getInstance() throws Exception {
+        if (singleton == null)
+            throw new Exception("Encryption is not initialized");
+        return singleton;
+    }
 
-    public OutputStream encryptMessage(String message) throws IOException{
-		try {
-			OutputStream outputStream = new ByteArrayOutputStream();
-			SecretKey secKey = new SecretKeySpec(key, algoName);
-			Cipher aes = Cipher.getInstance(transformation);
-			aes.init(Cipher.ENCRYPT_MODE, secKey, new IvParameterSpec(key));
-			byte[] encryptedArray = aes.doFinal(message.getBytes());
-			outputStream.write(encryptedArray);
-			return  outputStream;
-		}catch (NoSuchPaddingException | InvalidAlgorithmParameterException | BadPaddingException |
-				InvalidKeyException | NoSuchAlgorithmException | IllegalBlockSizeException ex){
-			throw new IOException("Error happened with encryption the message");
-		}
+    public OutputStream encryptMessage(String message) throws IOException {
+        try {
+            OutputStream outputStream = new ByteArrayOutputStream();
+            SecretKey secKey = new SecretKeySpec(key, algoName);
+            Cipher aes = Cipher.getInstance(transformation);
+            aes.init(Cipher.ENCRYPT_MODE, secKey, new IvParameterSpec(key));
+            byte[] encryptedArray = aes.doFinal(message.getBytes());
+            outputStream.write(encryptedArray);
+            return outputStream;
+        } catch (NoSuchPaddingException | InvalidAlgorithmParameterException | BadPaddingException |
+                InvalidKeyException | NoSuchAlgorithmException | IllegalBlockSizeException ex) {
+            throw new IOException("Error happened with encryption the message");
+        }
     }
 
     public String decryptMessage(InputStream inputStream) throws IOException {
-		try {
-			byte[] encryptedArray = readBytes(inputStream);
-			SecretKey secKey = new SecretKeySpec(key, algoName);
-			Cipher aes = Cipher.getInstance(transformation);
-			aes.init(Cipher.DECRYPT_MODE, secKey, new IvParameterSpec(key));
-			byte[] decryptedBytes = aes.doFinal(encryptedArray);
-			return new String(decryptedBytes);
-		}catch (NoSuchPaddingException | InvalidAlgorithmParameterException | BadPaddingException |
-				InvalidKeyException | NoSuchAlgorithmException | IllegalBlockSizeException ex){
-			throw new IOException("Error happened with decryption the message");
-		}
+        try {
+            byte[] encryptedArray = readBytes(inputStream);
+            SecretKey secKey = new SecretKeySpec(key, algoName);
+            Cipher aes = Cipher.getInstance(transformation);
+            aes.init(Cipher.DECRYPT_MODE, secKey, new IvParameterSpec(key));
+            byte[] decryptedBytes = aes.doFinal(encryptedArray);
+            return new String(decryptedBytes);
+        } catch (NoSuchPaddingException | InvalidAlgorithmParameterException | BadPaddingException |
+                InvalidKeyException | NoSuchAlgorithmException | IllegalBlockSizeException ex) {
+            throw new IOException("Error happened with decryption the message");
+        }
     }
 
-    private byte[] readBytes(InputStream inputStream) throws IOException{
-		ByteArrayOutputStream os = new ByteArrayOutputStream();
-		byte[] buffer = new byte[0xFFFF];
-		for (int len = inputStream.read(buffer); len != -1; len = inputStream.read(buffer)) {
-			os.write(buffer, 0, len);
-		}
-		return os.toByteArray();
-	}
+    private byte[] readBytes(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        byte[] buffer = new byte[0xFFFF];
+        for (int len = inputStream.read(buffer); len != -1; len = inputStream.read(buffer)) {
+            os.write(buffer, 0, len);
+        }
+        return os.toByteArray();
+    }
 }
--- build/commons/src/main/java/de/upb/cs/bibifi/commons/impl/Utilities.java
+++ build/commons/src/main/java/de/upb/cs/bibifi/commons/impl/Utilities.java
@@ -1,16 +1,20 @@
 package de.upb.cs.bibifi.commons.impl;
 
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 
 public class Utilities {
 
-    public static InputStream CovertString(String str){
+    public static InputStream convertString(String str) {
         byte[] array = str.getBytes(StandardCharsets.ISO_8859_1);
-        return  new ByteArrayInputStream(array);
+        return new ByteArrayInputStream(array);
     }
-    public static String ConvertOutputStream(OutputStream outputStream){
+
+    public static String convertOutputStream(OutputStream outputStream) {
         byte[] array = ((ByteArrayOutputStream) outputStream).toByteArray();
-        return  new String(array, StandardCharsets.ISO_8859_1);
+        return new String(array, StandardCharsets.ISO_8859_1);
     }
 }
