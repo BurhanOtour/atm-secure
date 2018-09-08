221df9655341df5609aa881ee2d255e5d93363cc
Rajat Khanna <rajat@mail.uni-paderborn.de>
2018-09-08 02:02:13 +0200

    Update ClientConnection.java (removed an unnecessary comment)

--- build/socket/src/main/java/de/upb/cs/bibifi/client/ClientConnection.java
+++ build/socket/src/main/java/de/upb/cs/bibifi/client/ClientConnection.java
@@ -5,12 +5,7 @@ import java.net.Socket;
 
 
 public class ClientConnection {
-    /**
-     * Runs the client as an application.  First it displays a dialog
-     * box asking for the IP address or hostname of a host running
-     * the date server, then connects to it and displays the date that
-     * it serves.
-     */
+    
     public static void main(String [] args) throws IOException {
         clientConnect(3000, "127.0.0.1");
     }
